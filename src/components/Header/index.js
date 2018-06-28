1ad4c4967055e54fd0ef9dcf7236e99617c7a84e
Jacob Roush <jacobroushmail@gmail.com>
2018-06-27 20:04:24 -0500

    added all header nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import './header.css'
+
 const Header = () => (
   <div
     style={{
@@ -16,17 +18,26 @@ const Header = () => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Roush
+        <Link to="/" style={{color: 'white',textDecoration: 'none',}}>
+          Roush.io
         </Link>
+        <Link className="header-link" to="/video">
+        <h1>Video</h1>
+        </Link>
+        <Link to="/web">
+        <h1>Web</h1>
+        </Link>
+        <Link to="/blog">
+        <h1>Blog</h1>
+        </Link>
+        <Link to="/about">
+        <h1>About</h1>
+        </Link>
+      
       </h1>
+      
     </div>
+    
   </div>
 )
 
