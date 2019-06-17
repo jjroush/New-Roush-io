d996406482a93536eb3163c1d1468ecdbbee5145
Jacob Roush <jacobroushmail@gmail.com>
2019-06-16 23:37:51 -0500

    video page

---
+++ src/pages/video.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const VideoPage = props => (
+  <>
+    <h1>Videography</h1>
+    <div>Because software isn't the only thing I like to create</div>
+    <iframe
+      src="https://player.vimeo.com/video/227504829"
+      width="640"
+      height="360"
+      frameborder="0"
+      allow="autoplay; fullscreen"
+      allowfullscreen
+    />
+  </>
+)
+
+export default VideoPage
