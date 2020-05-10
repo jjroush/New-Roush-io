2aa1addf37d1a585baecb38bb77b66059021fbc7
jjroush <jacobroushmail@gmail.com>
2020-05-10 01:04:56 -0500

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,38 +23,34 @@ export default class aboutPage extends Component {
           <h1>About</h1>
           <p>Hey, I'm Jacob Roush.</p>
           <p>
-            I am a Software Engineer in Iowa working for Hy-Vee, a midwestern grocery store chain with over 260 supermarkets.
-            As a developer I am always learning, particarly in the area of JavaScript and the general web development space.
+            I'm a Software Engineer in Iowa working for Hy-Vee, a midwestern grocery store chain with over 260 supermarkets.
           </p>
           <p>
-            I am currently focused on working with techonlogies like Node.js, Next.js, and Serverless.
+            Currently, I'm focused on working with technologies like Node.js, Next.js, and Serverless.
             I do my best to share what I've learned through Github and blog posts.
           </p>
-          <h3>More About Me</h3>
+          <h3>Even More About Me</h3>
           <p>
-            I've been doing Web Development for {devYears()} years. I've
-            always had an interest in technology.
+            I've been doing Web Development for {devYears()} years. 
+            The fast-paced nature of web development has kept me on my toes to learn new things. 
+            The ability for me to solve problems via code has motivated me to choose software engineering as a career.
+            I started learning in high school and through internships, college, and my fulltime career; I have continued to learn, make mistakes, and improve.
           </p>
         </div>
-
         <div>
+          <h4>When I am not at the computer</h4>
           <p>
-            I have a  Bachleor's in Managment of
-            Information Systems. The program was a perfect for me as it combines my
-            interest of business with learning how to solve these problems with technology.
+            I enjoy cooking.
+            There is a fun balance between art and science when it comes to cooking food.
+            The best dish I have prepared is gravlax, which is pretty much a fancy word for cured salmon on bread.
           </p>
           <p>
-            I am big fan of spicy food and am constantly on the lookout for new
-            hot sauces to try. My current is the Secret AardVark Habenero Hot
-            Sauce. I'm always looking for recommendations.
-          </p>
-          <p>
-            If you have any questions or are interested in working with me.{' '}
+            If you want to talk{' '}
             <a
               // eslint-disable-next-line
               onClick={this.toggleModal}
             >
-              Feel free to contact me.
+              reach out to me.
             </a>
           </p>
           <ContactModal show={this.state.isOpen} onClose={this.toggleModal} />
